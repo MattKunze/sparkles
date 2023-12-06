40c17607d06d160be697ed6e5340fbc47d247fbc
Matt Kunze <matt.kunze@gmail.com>
2023-12-06 16:52:04 -0700

    feat: default export final expression if possible

--- kernels/nodejs/performExecution.ts
+++ kernels/nodejs/performExecution.ts
@@ -2,6 +2,8 @@ import * as esbuild from "esbuild";
 import { readFile, writeFile } from "fs/promises";
 import { createRequire } from "module";
 import path from "path";
+import prettier from "prettier";
+import { parsers } from "prettier/plugins/typescript";
 import superjson from "superjson";
 import vm from "node:vm";
 
@@ -10,7 +12,12 @@ const require = createRequire(import.meta.url);
 export async function performExecution(filename: string) {
   try {
     const content = await readFile(filename, "utf-8");
-    const transformed = await esbuild.transform(content, {
+    const formatted = await prettier.format(content, {
+      parser: "typescript",
+      plugins: [defaultExportPlugin],
+    });
+
+    const transformed = await esbuild.transform(formatted, {
       format: "cjs",
       sourcemap: true,
     });
@@ -36,3 +43,25 @@ function createContext() {
     module: { exports: {} },
   };
 }
+
+const defaultExportPlugin: prettier.Plugin = {
+  parsers: {
+    typescript: {
+      ...parsers.typescript,
+      parse: (text, options) => {
+        const ast = parsers.typescript.parse(text, options);
+
+        // convert final expression to a default export if possible
+        const finalStatement = ast.body[ast.body.length - 1];
+        if (finalStatement.type === "ExpressionStatement") {
+          finalStatement.type = "ExportDefaultDeclaration";
+          finalStatement.exportKind = "value";
+          finalStatement.declaration = finalStatement.expression;
+          delete finalStatement.expression;
+        }
+
+        return ast;
+      },
+    },
+  },
+};
