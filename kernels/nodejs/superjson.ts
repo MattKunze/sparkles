546ce8ccad9f528f38d7165c917a49c9d9062408
Matt Kunze <matt.kunze@gmail.com>
2024-01-18 18:27:30 -0700

    feat: experiment with serializing object type

--- kernels/nodejs/superjson.ts
+++ kernels/nodejs/superjson.ts
@@ -1,9 +1,7 @@
 import SuperJSON from "superjson";
 
 export function isPromise(value: unknown): value is Promise<unknown> {
-  return (
-    value instanceof Promise || (value as any)?.constructor?.name === "Promise"
-  );
+  return value instanceof Promise;
 }
 
 SuperJSON.registerCustom<Promise<unknown>, string>(
@@ -26,6 +24,65 @@ SuperJSON.registerCustom<RegExp, string>(
     },
   },
   "regexp"
+);
+
+// hacking arround surrealdb types for now - this is deeply self-referential
+class Timeout {}
+SuperJSON.registerCustom<{}, string>(
+  {
+    isApplicable: (v): v is {} => v?.constructor?.name === "Timeout",
+    serialize: (v) => "",
+    deserialize: (v) => new Timeout() as {},
+  },
+  "surreal-timeout"
+);
+
+// this serializes named objects such that we rebuild POJOs but with the
+// correct class/constructor name
+// not pretty, but all the ugly stuff is pretty self-contained here
+const CustomFactories: Record<string, new (values: unknown) => any> = {};
+function createFactory(name: string) {
+  const f = new Function("values", "Object.assign(this, values)");
+  Object.defineProperty(f, "name", { value: name, writable: false });
+  return f as new (values: unknown) => unknown;
+}
+const BuiltIns = [
+  "Object",
+  "Array",
+  "Date",
+  "RegExp",
+  "Set",
+  "Map",
+  "URL",
+  "Error",
+  "InternalError",
+  "RangeError",
+  "ReferenceError",
+  "SyntaxError",
+  "TypeError",
+  "URIError",
+];
+SuperJSON.registerCustom<{}, string>(
+  {
+    isApplicable: (v): v is {} =>
+      typeof v === "object" &&
+      v?.constructor?.name &&
+      !("__typename" in v) &&
+      !BuiltIns.includes(v.constructor.name),
+    serialize: (v) =>
+      SuperJSON.stringify({
+        __typename: v.constructor.name,
+        ...v,
+      }),
+    deserialize: (v) => {
+      const { __typename, ...rest } = SuperJSON.parse<any>(v);
+      if (!(__typename in CustomFactories)) {
+        CustomFactories[__typename] = createFactory(__typename);
+      }
+      return new CustomFactories[__typename](rest);
+    },
+  },
+  "named-object"
 );
 
 export default SuperJSON;
--- src/components/molecules/ExportsTable/index.tsx
+++ src/components/molecules/ExportsTable/index.tsx
@@ -1,14 +1,16 @@
-import { isEmpty } from "lodash";
+import { isEmpty, isEqual } from "lodash";
 
 import { ExecutionDeferredResult } from "@/types";
 import { formatDuration } from "@/utils/format";
+
+const EMPTY_EXPORTS = { default: undefined };
 
 type Props = {
   data: Record<string, unknown>;
   deferred?: ExecutionDeferredResult["deferred"];
 };
 export function ExportsTable({ data, deferred }: Props) {
-  if (isEmpty(data)) {
+  if (isEmpty(data) || isEqual(data, EMPTY_EXPORTS)) {
     return (
       <div className="alert">
         <span className="font-mono">Empty results</span>
@@ -18,22 +20,33 @@ export function ExportsTable({ data, deferred }: Props) {
   return (
     <table className="table table-sm w-full">
       <tbody>
-        {Object.entries(data).map(([key, value]) => {
-          const { formatted, type } = decodeResult(key, value, deferred);
+        {Object.entries(data)
+          .sort(keySort)
+          .map(([key, value]) => {
+            const { formatted, type } = decodeResult(key, value, deferred);
 
-          return (
-            <tr key={key}>
-              <th className="font-mono">{key}</th>
-              <td className="w-full">
-                <pre>{formatted}</pre>
-              </td>
-              <td className="whitespace-nowrap">{type}</td>
-            </tr>
-          );
-        })}
+            return (
+              <tr key={key}>
+                <th className="font-mono">{key}</th>
+                <td className="w-full">
+                  <pre>{formatted}</pre>
+                </td>
+                <td className="whitespace-nowrap">{type}</td>
+              </tr>
+            );
+          })}
       </tbody>
     </table>
   );
+}
+
+// sort "default" first, then alphabetically
+function keySort([lhs]: [string, unknown], [rhs]: [string, unknown]) {
+  if (lhs === "default") {
+    return -1;
+  } else if (rhs === "default") {
+    return 1;
+  } else return lhs.localeCompare(rhs);
 }
 
 function decodeResult(
@@ -41,24 +54,21 @@ function decodeResult(
   value: unknown,
   deferred?: ExecutionDeferredResult["deferred"]
 ): { formatted: string; type: string } {
-  let formatted = JSON.stringify(value, null, 2);
-  let type: string = typeof value;
+  let formatted =
+    value === undefined ? "undefined" : JSON.stringify(value, null, 2);
+  let type: string =
+    // intended double equals to catch undefined and null
+    value == null ? formatted : value.constructor.name;
 
-  if (Array.isArray(value)) {
-    type = "array";
-  } else if (value instanceof Date) {
-    type = "date";
-  } else if (value instanceof Promise) {
+  if (value instanceof Promise) {
     const d = deferred?.[key];
     if (d) {
       type = `${d.result} (${formatDuration(d.duration, "human")})`;
       formatted = decodeResult(key, d.data).formatted;
     } else {
-      type = "promise";
       formatted = "<pending>";
     }
   } else if (value instanceof RegExp) {
-    type = "regexp";
     formatted = value.toString();
   }
   return { formatted, type };
